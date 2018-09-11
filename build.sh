1baede855fccd79ba661d7113f72ce9c43df10a3
AndrePietsch <andre.pietsch@scai.de>
2018-09-11 09:19:41 +0200

    comment

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
- export e2cid=12-34-56-78
- export installcode=asdfghjklqwertzu1234
+# export e2cid=12-34-56-78
+# export installcode=asdfghjklqwertzu1234
 docker-compose down && docker rmi easy2connect_easy2connectbox:latest && docker-compose rm && docker-compose build
