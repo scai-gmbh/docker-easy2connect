41d9a3de8f53aad3c9d4660107a9d79345ff0cfa
AndrePietsch <andre.pietsch@scai.de>
2019-11-16 18:23:56 +0100

    Allow building when no previously build image exists

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 # export e2cid=12-34-56-78
 # export installcode=asdfghjklqwertzu1234
-docker-compose down && docker rmi easy2connect_easy2connectbox:latest && docker-compose rm && docker-compose build
+echo "Make sure nothing is running"
+docker-compose down
+echo "Remove eventually existing image"
+docker rmi easy2connect_easy2connectbox:latest
+docker-compose rm
+echo "Building..."
+docker-compose build
+echo "To start run: docker-compose up -d"
